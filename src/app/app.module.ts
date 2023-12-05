0cb717372df1715986fcf27cbf5e3c9d95eb22d3
anatolii_filipenko <anatoliy.fil033@gmail.com>
2023-12-05 14:18:39 +0200

    initial commit with basic settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './components/product/product-detail/product-detail.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { TagManagementComponent } from './components/tag/tag-management/tag-management.component';
+import { TagListComponent } from './components/tag/tag-list/tag-list.component';
+import { TagEditComponent } from './components/tag/tag-edit/tag-edit.component';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent,
+    ProductListComponent,
+    TagManagementComponent,
+    TagListComponent,
+    TagEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
